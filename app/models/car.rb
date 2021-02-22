ab1ea0c4e02d953dc86f6e1b694b940c62edd04b
Teresa Chang <teresachang1@yahoo.co.uk>
2021-02-22 17:52:44 +0000

    add booking model and controller

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :bookings
   validates :price_per_day, presence: true, numericality: { only_integer: true }
   validates :type, presence: true
   validates :model, presence: true
