76edd3d168bc6a17e8d363f2a3a41301424c4ddf
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-23 16:16:15 +0000

    Booking form on car show page working

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :user
-  validates start_date, presence: true
-  validates total_days, presence: true, numericality: { only_integer: true }
-  validates total_price, presence: true, numericality: { only_integer: true }
+  validates :start_date, presence: true
+  validates :total_days, numericality: { only_integer: true }
+  validates :total_price, presence: true, numericality: { only_integer: true }
 end
