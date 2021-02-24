0b56899e9df6ae3aae645ca26c040f5075a08c91
Teresa Chang <teresachang1@yahoo.co.uk>
2021-02-24 12:29:28 +0000

    add cloudinary

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   validates :price_per_day, presence: true, numericality: { only_integer: true }
   validates :brand, presence: true
   validates :model, presence: true
