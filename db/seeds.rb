8f15ff3f37695e4933a97adb4d08b02c822cd979
Teresa Chang <teresachang1@yahoo.co.uk>
2021-02-24 14:39:55 +0000

    changed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,12 @@ response = Net::HTTP.get(uri)
 json = JSON.parse(response)
 
 puts 'Cleaning DB...'
+Booking.destroy_all
 Car.destroy_all
 User.destroy_all
-Booking.destroy_all
 
 puts 'Seeding new DB...'
-100.times do
+10.times do
   user = User.new(
     name: Faker::Name.name,
     email: Faker::Internet.email,
@@ -28,15 +28,14 @@ puts 'Seeding new DB...'
   )
   user.save!
 
-  randomIndex = rand(1..100)
+  randomIndex = rand(1..10)
   car = Car.new(
-    user_id: User.all.sample.id,
+    user: user,
     brand: json[randomIndex]["make"],
     model: json[randomIndex]["model"],
     price_per_day: randomIndex,
     img: json[randomIndex]["img_url"]
   )
   car.save!
-  puts "Created car: #{car.id}"
 
 end
