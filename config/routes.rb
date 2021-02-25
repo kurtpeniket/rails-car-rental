b5af8b42861d0390bf13d2df62d5fb5dbb5e760c
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-25 14:34:17 +0000

    Edit booking as user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :cars do
     resources :bookings, only: [:create]
   end
-  resources :bookings, only: [:show, :index]
+  resources :bookings, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
