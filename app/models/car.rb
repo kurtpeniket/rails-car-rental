a683214faf43592eefe8621eaaa39449fce6c4aa
Thomas Kenny <thomasarthurkenny@gmail.com>
2021-02-26 15:46:18 +0000

    Booking show page updated

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,7 +5,7 @@ class Car < ApplicationRecord
   validates :price_per_day, presence: true, numericality: { only_integer: true }
   validates :brand, presence: true
   validates :model, presence: true
-  validates :address, presence: true
+  # validates :address, presence: true
   # Geocoder
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
